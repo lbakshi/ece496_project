0442df9be6028d5d6b2eab86b0005b4ff3cb8de1
lbakshi <lakshb429@gmail.com>
2021-03-11 13:09:39 -0800

    added util and title to history screen

---
+++ Strike Sock/Strike Sock/util.swift
@@ -0,0 +1,20 @@
+//
+//  util.swift
+//  Strike Sock
+//
+//  Created by Lakshya Bakshi on 3/10/21.
+//
+
+import Foundation
+
+func stringFromDate(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "dd MMM yyyy HH:mm" //yyyy
+    return formatter.string(from: date)
+}
+
+func dateFromtString(_ str: String) -> Date {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "dd MMM yyyy HH:mm"
+    return formatter.date(from: str)!
+}
