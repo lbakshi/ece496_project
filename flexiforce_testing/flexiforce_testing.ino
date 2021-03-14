bb531594d2e9aecff533e95738bd41112cca5daa
tpabby <tp156@duke.edu>
2021-03-14 14:26:54 -0400

    adding BLE code to arduino IDE

--- flexiforce_testing/flexiforce_testing.ino
+++ flexiforce_testing/flexiforce_testing.ino
@@ -1,3 +1,7 @@
+#include <bluefruit.h>
+//Service UUID is 0x181D
+//Weight measurement characteristic UUID is 0x2A9D 
+//Weight Scale Feature characteristic UUID is 0x2A9E
 float cf = 19.5;
 int fs1 = A0;
 int fs2 = A1;
@@ -6,7 +10,12 @@ int toe = 0;
 int ball = 0;
 int heel = 0;
 float vout;
-unsigned long time = 0;
+unsigned long time = 0; 
+BLEService weight_service = BLEService(0x181D);
+BLECharacteristic weight_measurement_characteristic = BLECharacteristic(0x2A9D);
+BLECharacteristic weight_scale_feature = BLECharacteristic(0x2A9E);
+BLEDis bledis; // Device information service helper class instance???
+BLEBas blebas; // battery service helper class instance
 
 
 void setup() {
@@ -15,7 +24,54 @@ void setup() {
   pinMode(fs1, INPUT);
   pinMode(fs2, INPUT);
   pinMode(fs3, INPUT);
+  
+  // Initialize the Bluefruit module
+  Bluefruit.begin();
+  
+  // Set the advertised device name 
+  Bluefruit.setName("StrikeSock Bluefruit");
+  
+  // Set the connect/disconnect callback handlers 
+  Bluefruit.Periph.setConnectCallback(connect_callback);
+  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+  
+  // Configure and start the device information service 
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather52");
+  bledis.begin();
+  
+  // Start the BLE battery service and set it to 100%
+  blebas.begin();
+  blebas.write(100);
 
+  // Set up the weight scale service 
+  setUpWeight();
+  
+  // Set up the advertisign packets
+  startAdv();
+}
+
+void setUpWeight(void){
+  
+}
+
+void startAdv(void){
+  
+}
+
+void connect_callback(uint16_t conn_handle)
+{
+  
+}
+ 
+/**
+ * Callback invoked when a connection is dropped
+ * @param conn_handle connection where this event happens
+ * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
+ */
+void disconnect_callback(uint16_t conn_handle, uint8_t reason)
+{
+  
 }
 
 void loop() {
