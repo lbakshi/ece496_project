91c3cbdde8c6851e2eee70ca468039ef842f9b7a
lbakshi <lakshb429@gmail.com>
2021-03-16 16:26:54 -0700

    Revert "Merge branch 'revertedAnnaBranch'"
    This reverts commit f76f33377a2f77621d6018e6efb88f6a54e84951, reversing
    changes made to 20f6df7383fa2b4bdb0a15eb6a0a761a6a4beb67.

--- Strike Sock/Strike Sock/HardwarePeripheral.swift
+++ Strike Sock/Strike Sock/HardwarePeripheral.swift
@@ -11,8 +11,9 @@ import CoreBluetooth
 
 class HardwarePeripheral : NSObject {
     
-    public static let serviceUUID = CBUUID.init(string: "b4250400-fb4b-4746-b2b0-93f0e61122c6")
-    public static let frontCharUUID = CBUUID.init(string: "b4250401-fb4b-4746-b2b0-93f0e61122c6")
+    public static let serviceUUID = CBUUID.init(string: "181d")
+    public static let frontCharUUID = CBUUID.init(string: "2a9d")
+    public static let frontCharScaleUUID = CBUUID.init(string: "2a9e")
     public static let midCharUUID = CBUUID.init(string: "b4250402-fb4b-4746-b2b0-93f0e61122c6")
     public static let backCharUUID = CBUUID.init(string: "b4250403-fb4b-4746-b2b0-93f0e61122c6")
 }
