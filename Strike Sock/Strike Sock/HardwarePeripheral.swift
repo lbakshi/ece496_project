4d82fc11617710f12d3770781df09708a3085af9
lbakshi <lakshb429@gmail.com>
2021-03-23 12:57:08 -0700

    changed UUIDs temporarily

--- Strike Sock/Strike Sock/HardwarePeripheral.swift
+++ Strike Sock/Strike Sock/HardwarePeripheral.swift
@@ -11,8 +11,8 @@ import CoreBluetooth
 
 class HardwarePeripheral : NSObject {
     
-    public static let serviceUUID = CBUUID.init(string: "dea53006-ea01-4939-a384-1573aae78dca")
-    public static let frontCharUUID = CBUUID.init(string: "e7725f91-84af-4527-b045-6f7d3cc1b67d")
-    public static let midCharUUID = CBUUID.init(string: "ab77fad9-d27b-4c46-8b44-771be5c01072")
+    public static let serviceUUID = CBUUID.init(string: "0x180D")//dea53006-ea01-4939-a384-1573aae78dca")
+    public static let frontCharUUID = CBUUID.init(string: "0x2A37")//e7725f91-84af-4527-b045-6f7d3cc1b67d")
+    public static let midCharUUID = CBUUID.init(string: "0x2A38")//ab77fad9-d27b-4c46-8b44-771be5c01072")
     public static let backCharUUID = CBUUID.init(string: "1c005070-4bbd-428e-86bd-0287d7775a1e")
 }
