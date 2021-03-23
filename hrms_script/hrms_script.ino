47a66ede3dd5c803acfd3853949f5fd33f41c597
tpabby <tp156@duke.edu>
2021-03-23 15:49:40 -0400

    new arduino scripts

--- hrms_script/hrms_script.ino
+++ hrms_script/hrms_script.ino
@@ -1,12 +1,6 @@
 #include <bluefruit.h>
 #include <BLEUUID.h>
 
-// Online generated UUIDs in case we want to implement our own service and characteristics 
-//dea53006-ea01-4939-a384-1573aae78dca
-//e7725f91-84af-4527-b045-6f7d3cc1b67d
-//ab77fad9-d27b-4c46-8b44-771be5c01072
-//1c005070-4bbd-428e-86bd-0287d7775a1e
-
 int fs1 = A0;
 int fs2 = A1;
 int fs3 = A2;
@@ -15,41 +9,34 @@ int ball = 0;
 int heel = 0;
 unsigned long time = 0;
 
-//BLEUuid service_id = BLEUuid("dea53006-ea01-4939-a384-1573aae78dca");
-const uint8_t service_id[16] = {0xDE, 0xA5, 0x30, 0x06, 0xEA, 0x01, 0x49, 0x39, 0xA3, 0x84, 0x15, 0x73, 0xAA, 0xE7, 0x8D, 0xCA};
-const uint8_t toe_sensor_id[16] = {0xe7, 0x72, 0x5f, 0x91, 0x84, 0xaf, 0x45, 0x27, 0xb0, 0x45, 0x6f, 0x7d, 0x3c, 0xc1, 0xb6, 0x7d};
-const uint8_t midfoot_sensor_id[16] = {0xab, 0x77, 0xfa, 0xd9, 0xd2, 0x7b, 0x4c, 0x46, 0x8b, 0x44, 0x77, 0x1b, 0xe5, 0xc0, 0x10, 0x72};
-const uint8_t heel_sensor_id[16] = {0x1c, 0x00, 0x50, 0x70, 0x4b, 0xbd, 0x42, 0x8e, 0x86, 0xbd, 0x02, 0x87, 0xd7, 0x77, 0x5a, 0x1e};
-uint8_t notify_num = 0x10;
+/* HRM Service Definitions
+ * Heart Rate Monitor Service:  0x180D
+ * Heart Rate Measurement Char: 0x2A37
+ * Body Sensor Location Char:   0x2A38
+ */
+BLEService        hrms = BLEService(UUID16_SVC_HEART_RATE);
+BLECharacteristic hrmc = BLECharacteristic(UUID16_CHR_HEART_RATE_MEASUREMENT); // notify
+BLECharacteristic bslc = BLECharacteristic(UUID16_CHR_BODY_SENSOR_LOCATION); // read
 
-BLEService my_service = BLEService(BLEUuid(service_id));
-BLECharacteristic toe_sensor = BLECharacteristic(BLEUuid(toe_sensor_id));
-BLECharacteristic midfoot_sensor = BLECharacteristic(BLEUuid(midfoot_sensor_id));
-BLECharacteristic heel_sensor = BLECharacteristic(BLEUuid(heel_sensor_id));
-BLEDis bledis;
-BLEBas blebas;
+BLEDis bledis;    // DIS (Device Information Service) helper class instance
+BLEBas blebas;    // BAS (Battery Service) helper class instance
 
 void setUpWeight(void){
-  my_service.begin();
+  hrms.begin();
 
-  toe_sensor.setProperties(CHR_PROPS_NOTIFY);
-  toe_sensor.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  toe_sensor.setFixedLen(2);
-  toe_sensor.begin();
-  Serial.println("BEFORE NOTIFY");
-  //toe_sensor.notify32(toe);
+  hrmc.setProperties(CHR_PROPS_NOTIFY);
+  hrmc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  hrmc.setFixedLen(2);
+  //hrmc.setCccdWriteCallback(cccd_callback);  // Optionally capture CCCD updates
+  hrmc.begin();
+  uint8_t hrmdata[2] = { 0b00000110, 0x40 }; // Set the characteristic to use 8-bit values, with the sensor connected and detected
+  hrmc.write32(toe); 
 
-  midfoot_sensor.setProperties(CHR_PROPS_NOTIFY);
-  midfoot_sensor.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  midfoot_sensor.setFixedLen(2);
-  midfoot_sensor.begin();
-  //midfoot_sensor.notify32(ball);
-
-  heel_sensor.setProperties(CHR_PROPS_NOTIFY);
-  heel_sensor.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  heel_sensor.setFixedLen(2);
-  heel_sensor.begin();
-  //heel_sensor.notify32(heel);
+  bslc.setProperties(CHR_PROPS_READ);
+  bslc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  bslc.setFixedLen(1);
+  bslc.begin();
+  bslc.write8(6);
 
 }
 
@@ -59,7 +46,7 @@ void startAdv(void){
   Bluefruit.Advertising.addTxPower();
 
   // Include Weight Service UUID
-  Bluefruit.Advertising.addService(my_service);
+  Bluefruit.Advertising.addService(hrms);
 
   // Include name
   Bluefruit.Advertising.addName(); // should there be a name here?
@@ -109,11 +96,10 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason)
   Serial.println("Advertising!");
   
 }
+ 
 
 void setup() {
   // put your setup code here, to run once:
-  // put your setup code here, to run once:
-  Serial.println("AT BEGINNING OF SETUP");
   Serial.begin(9600);
   pinMode(fs1, INPUT);
   pinMode(fs2, INPUT);
@@ -144,6 +130,7 @@ void setup() {
   // Set up the weight scale service 
   setUpWeight();
 
+
 }
 
 void loop() {
@@ -151,8 +138,16 @@ void loop() {
   toe = analogRead(fs1);
   ball = analogRead(fs2);
   heel = analogRead(fs3);
+  //int me = 10/0;
 
   if(toe > 10){
+     delay(5000);
+     if(hrmc.notify32(toe)){
+      Serial.println("Toe sensor value updated");
+     }
+     else{
+      Serial.println("ERROR");
+     }
      Serial.print("Flexi Force sensor 1: ");
      Serial.print(toe);
      Serial.println("");
@@ -161,11 +156,13 @@ void loop() {
      Serial.print("Flexi Force sensor 2: ");
      Serial.print(ball);
      Serial.println("");
+     delay(1000);
   }
   if(heel > 10){
      Serial.print("Flexi Force sensor 3: ");
      Serial.print(heel);
      Serial.println("");
+     delay(1000);
   }
  
   delay(100);
