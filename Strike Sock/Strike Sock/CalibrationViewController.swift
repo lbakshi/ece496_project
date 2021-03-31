c0801a52e727ab9e5bd31cea258b8e936c300065
Anna Diemel <ad356@duke.edu>
2021-03-31 17:43:46 -0400

    Refactor into BLEViewController, Calibration and RunningView both connect to bluetooth

--- Strike Sock/Strike Sock/CalibrationViewController.swift
+++ Strike Sock/Strike Sock/CalibrationViewController.swift
@@ -9,51 +9,13 @@ import UIKit
 import CoreBluetooth
 import CorePlot
 
-class CalibrationViewController: UIViewController,
-                                 CBPeripheralDelegate,
-                                 CBCentralManagerDelegate{
-        
-    private var centralManager: CBCentralManager!
-    private var peripheral: CBPeripheral!
-    var runningSession : Session = Session()
-
-    @IBOutlet weak var calibrationStatus: UILabel!
-    @IBOutlet weak var frontStatus: UILabel!
-    @IBOutlet weak var middleStatus: UILabel!
-    @IBOutlet weak var backStatus: UILabel!
-    @IBOutlet weak var calibrationDescriptor: UILabel!
+class CalibrationViewController: BLEViewController {
     
-    @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var calibrationText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        calibrationStatus.text = "Initializing"
-        frontStatus.text = "No data"
-        middleStatus.text = "No data"
-        backStatus.text = "No data"
-        calibrationDescriptor.text = " "
-        cancelButton.isHidden = true
-        
-        centralManager = CBCentralManager(delegate: self, queue: nil)
-        
-    }
-    
-    func statusUpdate(_ text:String) {
-        calibrationStatus.text = text
-        print(text)
-    }
-    
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        // taken from RunningViewController, 3/31/21
-        statusUpdate("Central State update")
-        if central.state != .poweredOn {
-            statusUpdate("Central is not powered on")
-        } else {
-            statusUpdate("Central scanning for \(HardwarePeripheral.serviceUUID)");
-            centralManager.scanForPeripherals(withServices: [HardwarePeripheral.serviceUUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey : true])
-        }
+        calibrationText.text = " "
     }
     
     /*
