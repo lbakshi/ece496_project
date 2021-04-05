77fa674af2af0da434b739b347ec5afcd12dea42
lbakshi <lakshb429@gmail.com>
2021-04-04 18:01:30 -0700

    another characteristic change from 2acf to 2ad2

--- Strike Sock/Strike Sock/HardwarePeripheral.swift
+++ Strike Sock/Strike Sock/HardwarePeripheral.swift
@@ -14,6 +14,6 @@ class HardwarePeripheral : NSObject {
     public static let serviceUUID = CBUUID.init(string: "0x1826")//dea53006-ea01-4939-a384-1573aae78dca")
     public static let frontCharUUID = CBUUID.init(string: "0x2ACD")//e7725f91-84af-4527-b045-6f7d3cc1b67d")
     public static let midCharUUID = CBUUID.init(string: "0x2ACE")//ab77fad9-d27b-4c46-8b44-771be5c01072")
-    public static let backCharUUID = CBUUID.init(string: "0x2ACF")
+    public static let backCharUUID = CBUUID.init(string: "0x2AD2")
     public static let readUUID = CBUUID.init(string: "0x2ACC")
 }
