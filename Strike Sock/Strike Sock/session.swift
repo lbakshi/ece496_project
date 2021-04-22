258bf6b57feac46cdc25ace1bc09d5633a4141eb
lbakshi <lakshb429@gmail.com>
2021-04-21 20:52:03 -0700

    changing name

--- Strike Sock/Strike Sock/session.swift
+++ Strike Sock/Strike Sock/session.swift
@@ -49,12 +49,12 @@ class Session : Codable {
         self.startTime = Date()
         self.endTime = Date(timeIntervalSinceNow: range)
         for f in stride(from: 0, through: range, by: 0.5) {
-            lfrontArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 10...60)))
-            lmidArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 10...60)))
-            lbackArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 10...60)))
-            rfrontArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 10...60)))
-            rmidArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 10...60)))
-            rbackArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 10...60)))
+            lfrontArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 0...20)))
+            lmidArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 0...20)))
+            lbackArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 0...20)))
+            rfrontArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 0...20)))
+            rmidArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 0...20)))
+            rbackArr.append(dataPoint(time: Date(timeIntervalSinceNow: f), val: Double.random(in: 0...20)))
         }
     }
     
